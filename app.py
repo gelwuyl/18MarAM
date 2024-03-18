a1421a87498ef4c39ac308975be102ea30f84ba1
gelwuyl <163805686+gelwuyl@users.noreply.github.com>
2024-03-18 12:32:10 +0800

    Create app.py

---
+++ app.py
@@ -0,0 +1,31 @@
+pip install flask
+
+pip install google-generativeai
+
+from flask import Flask, request, render_template
+
+import google.generativeai as palm
+
+palm.configure(api_key="AIzaSyBgrPrnCD5XvXkFVavjqlVXQU9wnyY1AAA")
+model = {
+    "model": "models/chat-bison-001",
+}
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET","POST"])
+def index():
+    return(render_template("index.html"))
+
+@app.route("/palm", methods=["GET","POST"])
+def palm_flask():
+    q = request.form.get("q")
+    r = palm.chat(
+        **model,
+        messages=q
+    )
+    
+    return(render_template("reply.html",r=r.last))
+
+if __name__ == "__main__":
+    app.run(port=1234)
